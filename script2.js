3f346d6500b2457390a522ead77e443e5660eccc
e55562144 <e55562144@gmail.com>
2025-01-15 18:57:40 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,18 +2,3 @@
 document.getElementById('Yeter').addEventListener('click', () => {
     window.location.href = 'index.html';  // Sayfa yönlendirmesi
 });
-
-// İlk video bittiğinde ikinci videoyu başlatma işlemi
-document.getElementById("backgroundVideo").addEventListener("ended", function() {
-    var firstVideo = document.getElementById("backgroundVideo");
-    var secondVideo = document.getElementById("secondVideo");
-
-    // İlk videoyu gizle
-    firstVideo.style.display = "none";
-
-    // İkinci videoyu göster
-    secondVideo.style.display = "block";
-
-    // İkinci videoyu oynat
-    secondVideo.play();
-});
